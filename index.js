1370bee9f62eae735509b60716afe03ddb59c1f7
luco2Base <onlyjacyhere@gmail.com>
2024-09-26 13:12:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const https = require("https");
 
 const apiId = 28596369;
 const apiHash = "f50cfe3b10da015b2c2aa0ad31414a55";
-const sessionKey = "1BQANOTEuMTA4LjU2LjE2MgG7Fufep4JeHK10wncbW+mBf2bdOIAzf9usjoD/OKeKs6EUWp9agZzFRCfHxGIZ28crKkn3GkEim8K/8uhCljE3FnMF0FGK2Ps6EO81difpnNCWsXL9PpkgN3MIMi97sV6+bOSvd89iyIGv6nAfdgxzWB4gZrEv9ZkA+rl54O3dY8mpF+uQtNSZKrXTzzSJnKMKf8BAkyAaavJ1yS8H5GMdI9+6NJUHkfJfcY7Nqnn47fa4FOhT9kTDsf3o0HI0+i1mpa5aPMaS+HmFaqgDp6zTvirCLmdLloIqrp+ilnlZ3vQDguLPr/ertl/G5j9xgTc5fWZoCxYZ0pCtGJwtSelwDg=="
+const sessionKey = "1BQANOTEuMTA4LjU2LjE2MgG7nYhgq1uYJDBXEd7fNh0kOrxbW42XGF9eGRyYVcEa2kS/RlJThoIpJl4GoOAwXJs5maSe2sIOs0T4Qg51RuYVdRF80Gl/jCo1TsMBpSJ5pYOc+wYssZ61OF+dtrWIeXstRcrJazgIaJSWa9kqwaQbuhYYnQOmo0NaiVhOI50awaNR005RBuJ7OkWak/EWw+PfsdJ+cSmhR3Fh8rrENGqfk/gjsdMd6ruVpPo+Gj0gBIB+lecbpIzRqv4D7oXWnRhMuTMd/1jczfiO/iBcHpJjsJU970D0Tm8JljqA45W8LI1Trq9ed9JzL93PJSWBs1XdpYVG1+4xBovjlxnS5du0tA=="
 const stringSession = new StringSession(sessionKey);
 
 const apiUrl = "https://colorwiz.cyou/mana/receive_red_packet";
